809f148d99eac7beeb32ded8d7d320a091dc6e83
Damien Papworth <damien.papworth@vibrantmedia.com>
2014-04-28 19:48:20 +0100

    Revert "Use avahi for service discovery."
    This reverts commit f631fe02841966368716b9a103df46e1abcc2293.
    
    zookeeper doesn't appear to like avahi. :(

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,8 +1,28 @@
-node 'zk01' {
-  class { 'avahi': }
+# Host definitions for Zookeeper and Kafka nodes
+host { 'zk01.lan':
+  ip => '192.168.10.10',
+  host_aliases => 'zk01',
+}
+host { 'zk02.lan':
+  ip => '192.168.10.11',
+  host_aliases => 'zk02',
+}
+host { 'zk03.lan':
+  ip => '192.168.10.12',
+  host_aliases => 'zk03',
+}
+host { 'kafka01.lan':
+  ip => '192.168.10.13',
+  host_aliases => 'kafka01',
+}
+host { 'kafka02.lan':
+  ip => '192.168.10.14',
+  host_aliases => 'kafka02',
+}
 
+node 'zk01' {
   class { 'zookeeper':
-    hosts => { 'zk01.local' => 1, 'zk02.local' => 2, 'zk03.local' => 3 },
+    hosts => { 'zk01.lan' => 1, 'zk02.lan' => 2, 'zk03.lan' => 3 },
     data_dir => '/var/lib/zookeeper',
   }
 
@@ -10,10 +30,8 @@ node 'zk01' {
 }
 
 node 'zk02' {
-  class { 'avahi': }
-
   class { 'zookeeper':
-    hosts => { 'zk01.local' => 1, 'zk02.local' => 2, 'zk03.local' => 3 },
+    hosts => { 'zk01.lan' => 1, 'zk02.lan' => 2, 'zk03.lan' => 3 },
     data_dir => '/var/lib/zookeeper',
   }
 
@@ -21,10 +39,8 @@ node 'zk02' {
 }
 
 node 'zk03' {
-  class { 'avahi': }
-
   class { 'zookeeper':
-    hosts => { 'zk01.local' => 1, 'zk02.local' => 2, 'zk03.local' => 3 },
+    hosts => { 'zk01.lan' => 1, 'zk02.lan' => 2, 'zk03.lan' => 3 },
     data_dir => '/var/lib/zookeeper',
   }
 
@@ -32,20 +48,16 @@ node 'zk03' {
 }
 
 node 'kafka01' {
-  class { 'avahi': }
-
   class { 'kafka':
     broker_id => '1',
-    zookeeper_connect => 'zk01.local:2181,zk02.local:2181,zk03.local:2181',
+    zookeeper_connect => 'zk01.lan:2181,zk02.lan:2181,zk03.lan:2181',
   }
 }
 
 node 'kafka02' {
-  class { 'avahi': }
-
   class { 'kafka':
     broker_id => '2',
-    zookeeper_connect => 'zk01.local:2181,zk02.local:2181,zk03.local:2181',
+    zookeeper_connect => 'zk01.lan:2181,zk02.lan:2181,zk03.lan:2181',
   }
 }
 
